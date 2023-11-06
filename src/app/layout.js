0ca1e76a2b41a0e93c72531547d98de2b388be8a
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2023-11-06 15:02:02 +0100

    Set up motion config settings

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,7 @@ import { LIGHT_TOKENS, DARK_TOKENS, BLOG_TITLE } from '@/constants';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import './styles.css';
+import RespectMotionConfig from '@/components/RespectMotionConfig';
 
 const mainFont = Work_Sans({
   subsets: ['latin'],
@@ -33,17 +34,19 @@ function RootLayout({ children }) {
   const theme = 'light';
 
   return (
-    <html
-      lang='en'
-      className={clsx(mainFont.variable, monoFont.variable)}
-      data-color-theme={theme}
-      style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}>
-      <body>
-        <Header theme={theme} />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <RespectMotionConfig>
+      <html
+        lang='en'
+        className={clsx(mainFont.variable, monoFont.variable)}
+        data-color-theme={theme}
+        style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}>
+        <body>
+          <Header theme={theme} />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </RespectMotionConfig>
   );
 }
 
